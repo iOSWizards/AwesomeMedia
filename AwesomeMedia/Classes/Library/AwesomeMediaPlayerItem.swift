e07e4dfe187decac60801c5c80f4d69e7f7d3ebe
Leonardo Vinicius Kaminski Ferreira <leonardo.ferreira@beautydate.com>
2017-06-14 15:34:26 +0800

    Fixes KVO problem at AVPlayerItem

--- AwesomeMedia/Classes/Library/AwesomeMediaPlayerItem.swift
+++ AwesomeMedia/Classes/Library/AwesomeMediaPlayerItem.swift
@@ -10,9 +10,21 @@ import AVFoundation
 
 /// Responsible for handling the KVO operations that happen on the AVPlayerItem.
 
+public enum AwesomeMediaPlayerItemKeyPaths: String {
+    case playbackLikelyToKeepUp = "playbackLikelyToKeepUp"
+    case playbackBufferFull = "playbackBufferFull"
+    case status = "status"
+}
+
 public class AwesomeMediaPlayerItem: AVPlayerItem {
     
     var observersKeyPath = [String: NSObject?]()
+    var keysPathArray:[String]!
+    
+    convenience init(url URL: URL, keysPathArray: [String]) {
+        self.init(url: URL)
+        self.keysPathArray = keysPathArray
+    }
     
     deinit {
         for (keyPath, observer) in observersKeyPath {
@@ -24,7 +36,15 @@ public class AwesomeMediaPlayerItem: AVPlayerItem {
     
     override public func addObserver(_ observer: NSObject, forKeyPath keyPath: String, options: NSKeyValueObservingOptions = [], context: UnsafeMutableRawPointer?) {
         super.addObserver(observer, forKeyPath: keyPath, options: options, context: context)
-        observersKeyPath[keyPath] = observer
+        if keysPathArray.contains(keyPath) {
+            let obj = observersKeyPath[keyPath] as? NSObject
+            if obj == nil {
+                observersKeyPath[keyPath] = observer
+            } else {
+                self.removeObserver(obj!, forKeyPath: keyPath)
+                observersKeyPath[keyPath] = observer
+            }
+        }
     }
     
     override public func removeObserver(_ observer: NSObject, forKeyPath keyPath: String, context: UnsafeMutableRawPointer?) {
