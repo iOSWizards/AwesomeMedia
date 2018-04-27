3c592cfbb3b525c6a31d7510775071d04d9b10e5
Evandro Harrison Hoffmann <evandro@itsdayoff.com>
2018-04-27 15:39:33 +0800

    Removed unnecessary code.

--- AwesomeMedia/Classes/Extensions/UIViewExtensions.swift
+++ AwesomeMedia/Classes/Extensions/UIViewExtensions.swift
@@ -102,23 +102,3 @@ extension UIView {
         return nil
     }
 }
-
-// MARK: - layout changes
-
-extension UIView {
-    public func updateLayout(animated: Bool, completion: (() -> Void)? = nil) {
-        if animated {
-            let translatesAutoresizingMaskIntoConstraintsBefore = translatesAutoresizingMaskIntoConstraints
-            translatesAutoresizingMaskIntoConstraints = true
-            UIView.animate(withDuration: 0.3, animations: {
-                self.layoutIfNeeded()
-            }, completion: { (_) in
-                self.translatesAutoresizingMaskIntoConstraints = translatesAutoresizingMaskIntoConstraintsBefore
-                completion?()
-            })
-        } else {
-            self.layoutIfNeeded()
-            completion?()
-        }
-    }
-}
